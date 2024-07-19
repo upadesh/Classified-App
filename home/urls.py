b74af15c1538e60a57154adc5fe91347671e751b
Upadesh Shrestha <upadesh@yahoo.com>
2024-07-19 17:10:14 +0545

    Few Changes

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CompanyClassifiedAdHomeListView
+# from .views import CompanyClassifiedAdHomeListView
+from .views import home
 
 urlpatterns = [  
-    path('', CompanyClassifiedAdHomeListView.as_view(), name='home'),
+    # path('', CompanyClassifiedAdHomeListView.as_view(), name='home'),
+    path('', home, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from django.views.generic import ListView
-from listing.models import ClassifiedAd
-from company.models import Company
+# from django.views.generic import ListView
+# from listing.models import ClassifiedAd
+# from company.models import Company
 
 # Create your views here.
-class CompanyClassifiedAdHomeListView(ListView):
-    model = ClassifiedAd
-    template_name = 'home/index.html'
-    context_object_name = 'ads'
+# class CompanyClassifiedAdHomeListView(ListView):
+#     model = ClassifiedAd
+#     template_name = 'home/index.html'
+#     context_object_name = 'ads'
         
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        for ad in context['ads']:
-            ad.company_name = ad.user.company.company_name
-            ad.logo = ad.user.company.logo
-        return context
-    
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         for ad in context['ads']:
+#             ad.company_name = ad.user.company.company_name
+#             ad.logo = ad.user.company.logo
+#         return context
+
+def home(request):
+    return render(request, 'index.html')
